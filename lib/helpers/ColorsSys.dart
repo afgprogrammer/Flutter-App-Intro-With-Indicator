8a4d83814d80a315ccfb7cb247aef09e85ddfdb2
Mohammad Rahmani <rto1680@gmail.com>
2020-05-13 21:55:29 +0430

    Add color system class

---
+++ lib/helpers/ColorsSys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = Color.fromRGBO(52, 43, 37, 1);
+  static Color gray = Color.fromRGBO(137, 137, 137, 1);
+  static Color secoundry = Color.fromRGBO(198, 116, 27, 1);
+  static Color secoundryLight = Color.fromRGBO(226, 185, 141, 1);
+}
