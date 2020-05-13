f26f4d2716af0c7f8bd83452977f014cb4da3aa3
Mohammad Rahmani <rto1680@gmail.com>
2020-05-13 21:55:41 +0430

    Add strings class

---
+++ lib/helpers/Strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static var stepOneTitle = "Farm Driving";
+  static var stepOneContent = "There are all kinds of equipment to build your farm better harvest";
+  static var stepTwoTitle = "Plant Growing";
+  static var stepTwoContent = "Be part of the agriculture and gives your team the  power you need to do your best";
+  static var stepThreeTitle = "Fast Harvesting";
+  static var stepThreeContent = "Your will be proud to be part of agriculture and it’s harvest";
+}
